48c7bbc5b651ddf2bcb80a52248ee995f500346d
Séamus <shinra.electric@icloud.com>
2024-01-28 16:20:35 +0100

    Add build script

---
+++ build_rpcs3.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env zsh
+
+# ANSI color codes
+PURPLE='\033[0;35m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# This gets the location that the script is being run from and moves there.
+SCRIPT_DIR=${0:a:h}
+cd "$SCRIPT_DIR"
+
+# Check if Homebrew is installed
+if ! command -v brew &> /dev/null; then
+	echo -e "${PURPLE}Homebrew not found. Installing Homebrew...${NC}"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	echo -e "${PURPLE}Homebrew found. Updating Homebrew...${NC}"
+	brew update
+fi
+
+## Homebrew dependencies
+# Install required dependencies
+echo -e "${PURPLE}Checking for Homebrew dependencies...${NC}"
+brew_dependency_check() {
+	if [ -d "$(brew --prefix)/opt/$1" ]; then
+		echo -e "${GREEN}Found $1. Checking for updates...${NC}"
+			brew upgrade $1
+	else
+		 echo -e "${PURPLE}Did not find $1. Installing...${NC}"
+		brew install $1
+	fi
+}
+
+deps=( cmake ffmpeg glew libusb llvm@17 molten-vk nasm ninja pkg-config qt@6 sdl2 vulkan-headers )
+
+for dep in $deps[@]
+do 
+	brew_dependency_check $dep
+done
+
+# Make sure glslang and cubeb(macOS 14) are uninstalled, otherwise building will fail
+# Will reinstall at the end of the script
+reinstall_glslang=false
+reinstall_cubeb=false
+
+brew_remove() {
+	echo -e "${PURPLE}Checking for $1 installation...${NC}"
+	if [ -d "$(brew --prefix)/opt/$1" ]; then
+		brew rm $1 && echo -e "${GREEN}Removing $1${NC}"
+		if [ "$1" = "glslang" ]; then 
+			reinstall_glslang=true
+		elif [ "$1" = cubeb ]; then
+			reinstall_cubeb=true
+		fi
+	else
+		echo -e "${GREEN}$1 not found${NC}"
+	fi
+}
+
+brew_remove glslang
+brew_remove cubeb
+
+# Set variables
+export Qt6_DIR=$(brew --prefix)/opt/qt6
+export VULKAN_SDK=$(brew --prefix)/opt/molten-vk
+if [ ! -h "$VULKAN_SDK/lib/libvulkan.dylib" ]; then 
+	echo -e "${PURPLE}Creating libvulkan.dylib symlink${NC}"
+	ln -s "$VULKAN_SDK/lib/libMoltenVK.dylib" "$VULKAN_SDK/lib/libvulkan.dylib"
+fi 
+export VK_ICD_FILENAMES=$VULKAN_SDK/share/vulkan/icd.d/MoltenVK_icd.json
+export LLVM_DIR=$(brew --prefix)/opt/llvm@17
+
+# Update Repository
+git clone https://github.com/shinra-electric/rpcs3
+cd rpcs3
+git remote add upstream https://github.com/RPCS3/rpcs3
+git pull upstream master
+git rebase origin/macos-fix
+
+# Update only the submodules that are needed
+git submodule update --init ./3rdparty/asmjit
+git submodule update --init ./3rdparty/cubeb
+git submodule update --init ./3rdparty/curl
+git submodule update --init ./3rdparty/flatbuffers
+git submodule update --init ./3rdparty/glslang
+git submodule update --init ./3rdparty/GPUOpen
+git submodule update --init ./3rdparty/hidapi
+git submodule update --init ./3rdparty/libusb
+git submodule update --init ./3rdparty/miniupnp
+git submodule update --init ./3rdparty/pine
+git submodule update --init ./3rdparty/pugixml
+git submodule update --init ./3rdparty/rtmidi
+git submodule update --init ./3rdparty/SoundTouch
+git submodule update --init ./3rdparty/SPIRV
+git submodule update --init ./3rdparty/stblib
+git submodule update --init ./3rdparty/wolfssl
+git submodule update --init ./3rdparty/xxHash
+git submodule update --init ./3rdparty/yaml-cpp
+#git submodule update --init ./3rdparty/libpng
+#git submodule update --init ./3rdparty/llvm
+#git submodule update --init ./3rdparty/FAudio
+
+# Fix hidapi
+sed -i '' "s/extern const double NSAppKitVersionNumber;/const double NSAppKitVersionNumber = 1343;/g" 3rdparty/hidapi/hidapi/mac/hid.c
+
+# Configure build system
+mkdir build && cd build
+cmake .. -DUSE_ALSA=OFF -DUSE_PULSE=OFF -DUSE_AUDIOUNIT=ON -DUSE_NATIVE_INSTRUCTIONS=OFF -DUSE_SYSTEM_FFMPEG=on -DCMAKE_OSX_ARCHITECTURES="arm64" -DLLVM_TARGETS_TO_BUILD="AArch64;ARM" -GNinja -DUSE_SYSTEM_MVK=on -DUSE_FAUDIO=OFF -DBUILD_LLVM=OFF -DUSE_SYSTEM_LIBPNG=ON -Wno-deprecated
+
+# Build
+ninja 
+
+# Codesign
+codesign --force --deep --sign - bin/rpcs3.app/Contents/MacOS/rpcs3
+
+# Check that the build was successful
+if [ $? -eq 0 ]; then
+	# Remove existing app
+	cd "$SCRIPT_DIR"
+	rm -rf RPCS3-Arm.app
+	
+	# Move app
+	cp -R ./rpcs3/build/bin/rpcs3.app ./RPCS3-Arm.app
+	
+	# Cleanup 
+	rm -rf rpcs3
+fi 
+
+# Reinstall glslang or cubeb if they were originally installed
+if [ $reinstall_glslang = true ]; then
+	echo -e "${PURPLE}Reinstalling glslang${NC}" 
+	brew install glslang
+fi
+
+if [ $reinstall_cubeb = true ]; then
+	echo -e "${PURPLE}Reinstalling cubeb${NC}"
+	brew install cubeb
+fi
