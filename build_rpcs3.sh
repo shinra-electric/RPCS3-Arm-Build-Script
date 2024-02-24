37c71ec21012467c2cb7642139126c6dec96020e
Séamus <shinra.electric@icloud.com>
2024-02-24 20:13:32 +0100

    Remove workaround for old-style-casts

--- build_rpcs3.sh
+++ build_rpcs3.sh
@@ -75,15 +75,6 @@ export LLVM_DIR=$(brew --prefix)/opt/llvm@17
 git clone https://github.com/RPCS3/rpcs3
 cd rpcs3
 
-# Suppress old-style-cast warnings
-sed -i '' '1i\'$'\n''#if defined(__clang__)'$'\n' ./darwin/util/sysinfo_darwin.mm
-sed -i '' '2i\'$'\n''#pragma clang diagnostic ignored "-Wold-style-cast"'$'\n' ./darwin/util/sysinfo_darwin.mm
-sed -i '' '3i\'$'\n''#endif'$'\n' ./darwin/util/sysinfo_darwin.mm
-
-sed -i '' '7i\'$'\n''#if defined(__clang__)'$'\n' ./rpcs3/display_sleep_control.cpp
-sed -i '' '8i\'$'\n''#pragma clang diagnostic ignored "-Wold-style-cast"'$'\n' ./rpcs3/display_sleep_control.cpp
-sed -i '' '9i\'$'\n''#endif'$'\n' ./rpcs3/display_sleep_control.cpp
-
 # Update only the submodules that are needed
 git submodule update --init ./3rdparty/asmjit
 git submodule update --init ./3rdparty/cubeb
